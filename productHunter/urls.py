d891df9a6efdee865ec7facf87892ba1d001bbfb
Momoh Nobert <okhai0112@gmail.com>
2020-07-03 00:46:22 +0100

    added the routing for create function and created the html

--- productHunter/urls.py
+++ productHunter/urls.py
@@ -20,5 +20,6 @@ from products import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('user/', include('user.urls'))
+    path('user/', include('user.urls')),
+    path('products/', include('products.urls')),
 ]
--- products/views.py
+++ products/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'products/home.html')
+
+
+def create(request):
+    return render(request, 'products/create.html')
