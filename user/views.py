e8612bad7f32aba670f976a33ee7bd4f23698d76
Momoh Nobert <okhai0112@gmail.com>
2020-07-02 17:28:03 +0100

    did the validation and POST with inbuilt Django configs

--- user/views.py
+++ user/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 
+signUpPage = 'user/signup.html'
+
+
 def signup(request):
-    return render(request, 'user/signup.html')
+    if request.method == 'POST':
+        # The user wants to sign up!
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, signUpPage, {'error': 'Username has already been taken!'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                print("Done! Created new user")
+                return redirect('home')
+        else:
+            return render(request, signUpPage, {'error': 'Passwords do not match!'})
+    else:
+        # User wants to enter info!
+        return render(request, signUpPage)
 
 
 def login(request):
@@ -14,4 +33,4 @@ def login(request):
 def logout(request):
     # TODO Need to route to homepage
     # don't forget logout
-    return render(request, 'user/signup.html')
+    return render(request, signUpPage)
